fec6b5b7ae329c8fa11e750b89b45c5b64472a75
foontinz <foontinz@protonmail.com>
2022-09-15 16:08:45 +0300

    new version

--- main.py
+++ main.py
@@ -3,39 +3,7 @@ import operator
 import gspread
 import requests
 import datetime
-
-
-def get_c2c_price(payment_method, limit):
-    url = "https://p2p.binance.com/bapi/c2c/v2/friendly/c2c/adv/search"
-    payload = {
-        "proMerchantAds": False,
-        "page": 1,
-        "rows": 2,
-        "payTypes": [payment_method],
-        "countries": [],
-        "publisherType": None,
-        "asset": "USDT",
-        "fiat": "RUB",
-        "transAmount": limit,
-        "tradeType": "BUY"
-    }
-    headers = {
-        "authority": "p2p.binance.com",
-        "accept": "*/*",
-        "accept-language": "ru,en-US;q=0.9,en;q=0.8,uk-UA;q=0.7,uk;q=0.6,ru-RU;q=0.5",
-        "clienttype": "web",
-        "content-type": "application/json",
-        "lang": "en",
-        "origin": "https://p2p.binance.com",
-        "sec-ch-ua-mobile": "?0",
-        "sec-ch-ua-platform": "Linux",
-        "sec-fetch-dest": "empty",
-        "sec-fetch-mode": "cors",
-        "user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36",
-        "sec-fetch-site": "same-origin"
-    }
-
-    return requests.request("POST", url, json=payload, headers=headers).json()['data'][0]['adv']['price']
+import string
 
 
 def get_spot_price(attempts=0):
@@ -49,124 +17,176 @@ def get_spot_price(attempts=0):
             return None
 
 
-def prepare_worksheet(timings, banks, worksheet):
-    worksheet.update("B2:E2", [banks])
-    worksheet.update("F2", "Limit")
-    worksheet.update("B6:G6", [timings])
-    print("Worksheet prepared. ")
+class BinanceC2CScraper:
+    def __init__(self, fiat, asset, limit, method, is_merchant, payment_methods):
+        self.fiat = fiat
+        self.asset = asset
+        self.limit = limit
+        self.is_merchant = is_merchant
+        self.method = method
+        self.payment_methods = payment_methods
+
+        self.data = self.make_c2c_requests()
+
+    def make_c2c_requests(self):
+        for payment_method in self.payment_methods:
+            url = "https://p2p.binance.com/bapi/c2c/v2/friendly/c2c/adv/search"
+            payload = {
+                "proMerchantAds": self.is_merchant,
+                "page": 1,
+                "rows": 2,
+                "payTypes": [payment_method],
+                "countries": [],
+                "publisherType": None,
+                "asset": self.asset,
+                "fiat": self.fiat,
+                "transAmount": self.limit,
+                "tradeType": self.method
+            }
+            headers = {
+                "authority": "p2p.binance.com",
+                "accept": "*/*",
+                "accept-language": "ru,en-US;q=0.9,en;q=0.8,uk-UA;q=0.7,uk;q=0.6,ru-RU;q=0.5",
+                "clienttype": "web",
+                "content-type": "application/json",
+                "lang": "en",
+                "origin": "https://p2p.binance.com",
+                "sec-ch-ua-mobile": "?0",
+                "sec-ch-ua-platform": "Linux",
+                "sec-fetch-dest": "empty",
+                "sec-fetch-mode": "cors",
+                "user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36",
+                "sec-fetch-site": "same-origin"
+            }
+
+            yield requests.request("POST", url, json=payload, headers=headers).json()['data']
+
+    def get_price(self):
+        try:
+            return float(next(self.data)[0]['adv']['price'])
+        except IndexError or AttributeError:
+            return 0.0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        return self.get_price()
 
 
-def get_current_cell(worksheet):
-    for row in range(7, 40):
-        for column in ['B', 'C', 'D', 'E', 'F', 'G']:
-            if not worksheet.acell(column + str(row)).value:
-                return column, str(row)
+class WorksheetInterface:
+    COLUMNS = string.ascii_uppercase
+    TIME_SECTORS = ["4:00", "8:00", "12:00", "16:00", "20:00", "24:00"]
+
+    def __init__(self, worksheet_name, spreadsheet, payment_methods):
+        self.worksheet_name = worksheet_name
+        self.worksheet = spreadsheet.worksheet(self.worksheet_name)
+        self.payment_methods = payment_methods
+        self.columns_payment = self.COLUMNS[1:len(self.payment_methods) + 1]
+        self.prepare_worksheet()
+
+    @staticmethod
+    def save_row(row, date):
+        with open("last.txt", 'w') as fw:
+            fw.write(f"{row},{date}")
+
+    @staticmethod
+    def load_row():
+        with open("last.txt", 'r') as fr:
+            return fr.read().split(',')
+
+    def prepare_worksheet(self):
+        self.worksheet.update(f"B1:{self.columns_payment[-1]}1", [self.payment_methods])
+        self.worksheet.update("A1", "Limit")
+        self.worksheet.update("B3:G3", [self.TIME_SECTORS])
+        print(f"Worksheet {self.worksheet_name} prepared.")
+
+    def update_payment_methods(self):
+        self.payment_methods = {method: None for method in self.payment_methods}
+        for column, (method, value) in enumerate(self.payment_methods.items()):
+            self.payment_methods.update({method: self.worksheet.cell(2, column + 1).value})
+
+    def get_limit(self):
+        return self.worksheet.acell('A2').value
+
+    def enter_into_cell(self, cell, value):
+        self.worksheet.update(cell, value)
+
+    def clear(self):
+        self.worksheet.clear()
+
+    def put_current_date(self, row):
+        self.enter_into_cell(f"A{row}", datetime.datetime.now().strftime('%d.%m'))
+        self.save_row(row, datetime.datetime.now().strftime('%Y-%m-%d').__str__())
+
+    def find_max_spread(self, row):
+        columns = ['B', 'C', 'D', 'E', 'F', 'G']
+        values = self.worksheet.get_values(f"B{row}:G{row}")[0]
+        values = [float(spread.replace(",", ".")) if spread else 0.0 for spread in values]
+
+        columns_dict = (dict(zip(columns, values)))
+        return max(columns_dict.items(), key=operator.itemgetter(1))[0]
+
+    def color_biggest_spread(self, row):
+        self.worksheet.format(f"{self.find_max_spread(row)}{row}", {
+            "backgroundColorStyle": {
+                "themeColor": "ACCENT3"}})
 
 
-def get_limit(worksheet):
-    return worksheet.acell('F3').value
+class MainRoot:
+    def __init__(self, wks_interface: WorksheetInterface):
+        self.wks_interface = wks_interface
+
+    def pass_cell(self, cell):
+        spot_price = get_spot_price()
+        self.wks_interface.update_payment_methods()
+        payment_methods = [payment_method for payment_method in self.wks_interface.payment_methods if
+                           self.wks_interface.payment_methods[payment_method]]
+        limit = self.wks_interface.get_limit()
+        if payment_methods:
+            spread = round(max(BinanceC2CScraper("RUB", "USDT", limit, "BUY", False, payment_methods)) - spot_price, 3)
+        else:
+            spread = 0
+        self.wks_interface.enter_into_cell(cell, spread)
+
+    def main_loop(self, row, date):
+        today = datetime.datetime.now().date()
+
+        while today.__str__() == date.__str__():
+            now = datetime.datetime.now()
+            self.wks_interface.put_current_date(row)
+
+            today = datetime.datetime.now().strftime('%Y-%m-%d')
+            hour = now.hour + now.minute / 60
+
+            timeframes = {4.0: "B", 8.0: "C", 12.0: "D", 16.0: "E", 20.0: "F", 24.0: "G"}
+            next_column = min([timeframe for timeframe in timeframes if hour < timeframe], key=lambda x: abs(x - hour))
+
+            print(f"{now.strftime('%d.%m %H:%M:%S')}. Sleeping for {int((next_column - hour) * 3600)} seconds")
+            time.sleep((next_column - hour) * 3600)
+            self.pass_cell(timeframes[next_column] + row)
+
+        self.wks_interface.color_biggest_spread(row)
+        future_date = (date + datetime.timedelta(days=1))
+        self.main_loop(str(int(row) + 1), future_date)
 
 
-def get_payment_methods(worksheet, initial_methods):
-    methods_dict = {}
-    for bank, cell in initial_methods.items():
-        cell = cell.split(',')
-        methods_dict.update({bank: worksheet.acell(''.join(cell[0] + str(int(cell[1]) + 1))).value})
-    return methods_dict
+def main(spreadsheet_name, filename="service_account.json"):
+    payment_methods = ["RosBank", "Tinkoff", "RaiffeisenBankRussia", "QIWI"]
+    service_account = gspread.service_account(filename=filename)
+    spreadsheet = service_account.open(spreadsheet_name)
 
-
-def enter_into_cell(worksheet, cell, value):
-    worksheet.update(cell, value)
-    save_last_cell(cell, datetime.datetime.now().strftime('%d.%m'))
-
-
-def save_last_cell(cell, date):
-    with open("last.txt", 'w') as fw:
-        cell_column = cell[0]
-        cell_row = cell[1:]
-        fw.write(f"{date},{cell_column},{cell_row}")
-
-
-def load_last_cell():
-    with open("last.txt", 'r') as fr:
-        return fr.read()
-
-
-def clear_sheet(worksheet):
-    worksheet.clear()
-
-
-def enter_current_date(worksheet, row):
-    enter_into_cell(worksheet, f"A{row}", datetime.datetime.now().strftime('%d.%m'))
-
-
-def find_biggest_spread(row, worksheet):
-    columns = ['B', 'C', 'D', 'E', 'F', 'G']
-    values = worksheet.get_values(f"B{row}:G{row}")[0]
-    values = [float(spread.replace(",", ".")) if spread else 0 for spread in values]
-    columns_dict = (dict(zip(columns, values)))
-    return max(columns_dict.items(), key=operator.itemgetter(1))[0]
-
-
-def color_biggest_spread(row, worksheet):
-    worksheet.format(f"{find_biggest_spread(7, worksheet)}{row}", {
-        "backgroundColor": {
-            "red": 100.0,
-            "green": 0.0,
-            "blue": 0.0
-        }})
-
-
-def day_loop(worksheet, payment_methods, row, date):
-    today = datetime.datetime.now().strftime('%d.%m')
-    enter_current_date(worksheet, row)
-    while today == date:
-        now = datetime.datetime.now()
-
-        today = datetime.datetime.now().strftime('%d.%m')
-        hour = now.hour + now.minute / 60
-
-        timeframes = {4.0: "B", 8.0: "C", 12.0: "D", 16.0: "E", 20.0: "F", 24.0: "G"}
-        next_iteration = min([timeframe for timeframe in timeframes if hour < timeframe], key=lambda x: abs(x - hour))
-        cell = timeframes[next_iteration] + row
-
-        print(f"{now.strftime('%d.%m %H:%M:%S')}. Sleeping for {int((next_iteration - hour) * 3600)} seconds")
-        time.sleep((next_iteration - hour) * 3600)
-        four_hours_loop(worksheet, cell, payment_methods)
-
-    color_biggest_spread(row, worksheet)
-    day_loop(worksheet, payment_methods, row + 1, datetime.datetime.now().strftime('%d.%m'))
-
-
-def four_hours_loop(worksheet, cell, pay_methods):
-    limit = get_limit(worksheet)
-    spot_price = get_spot_price()
-    methods = get_payment_methods(worksheet, pay_methods)
-    c2c_price = [float(get_c2c_price(method, limit)) for method in methods if methods[method]]
-    if c2c_price:
-        spread = round(max(c2c_price) - spot_price, 3)
-    else:
-        spread = 0
-
-    enter_into_cell(worksheet, cell, spread)
-
-
-def main_loop():
-    payment_methods = {"RosBank": "B,2", "Tinkoff": "C,2", "RaiffeisenBankRussia": "D,2", "QIWI": "E,2"}
-    time_holders = ["4:00", "8:00", "12:00", "16:00", "20:00", "24:00"]
-
-    sa = gspread.service_account(filename="service_account.json")
-    sh = sa.open("binance")
-    wks = sh.worksheet("main")
     while True:
-        prepare_worksheet(time_holders, list(payment_methods.keys()), wks)
-        last_cell = load_last_cell().split(",")
-        if last_cell[1] == '38':
-            clear_sheet(wks)
+        worksheet_interface = WorksheetInterface('main', spreadsheet, payment_methods)
+        root = MainRoot(worksheet_interface)
+
+        last_row = worksheet_interface.load_row()
+        if last_row[0] == '35':
+            worksheet_interface.clear()
             continue
         else:
-            day_loop(wks, payment_methods, last_cell[2], last_cell[0])
+            root.main_loop(last_row[0], datetime.datetime.strptime(last_row[1], '%Y-%m-%d').date())
 
 
 if __name__ == '__main__':
-    main_loop()
+    main("binance_tracker")
